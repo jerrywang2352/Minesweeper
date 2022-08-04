3524b61e1b079e05a9e5eb90a1bfcca9c7c4c427
Jerry Wang <jerry.wang2352@gmail.com>
2022-08-04 04:47:59 -0400

    Play game

--- main.py
+++ main.py
@@ -2,5 +2,5 @@ from tkinter import *
 from MineGrid import MineGrid
 
 root = Tk()
-game = MineGrid(root, 25, 25, 100) #plays a game of Minesweeper on a 25 x 25 grid with 100 bombs
+game = MineGrid(root, 25, 25, 75) #plays a game of Minesweeper on a 25 x 25 grid with 100 bombs
 root.mainloop()
